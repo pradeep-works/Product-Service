b65094b590e14b41724d4cf5d2675899b7125788
Pradeep <pradeep.mokara@volunteer.heartfulness.org>
2024-04-06 11:12:41 +0530

    feat: Add perms for methods

--- retest_app/views.py
+++ retest_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from rest_framework.routers import DefaultRouter
 
 from retest_app.models import Product
@@ -14,7 +14,14 @@ class ProductViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Retr
     '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [AllowAdminOnly]
+
+    def get_permissions(self):
+        if self.request.method.upper() in ['HEAD', 'OPTIONS']:
+            return []
+        if self.request.method.upper() in ['GET']:
+            return [IsAuthenticated()]
+        if self.request.method.upper() in ['POST', 'PATCH', 'PUT', 'DELETE']:
+            return [AllowAdminOnly()]
 
 router = DefaultRouter()
 router.register("product", ProductViewSet, "product")
