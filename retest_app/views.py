b42175c195f360a23cb4a092a957ff48075b1e6f
Pradeep <pradeep.mokara@volunteer.heartfulness.org>
2024-04-15 20:38:12 +0530

    chore: remove unwanted code

--- retest_app/views.py
+++ retest_app/views.py
@@ -6,6 +6,8 @@ from rest_framework.routers import DefaultRouter
 from retest_app.models import Product
 from retest_app.serializers import ProductSerializer
 from retest_app.permissions import AllowAdminOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 class ProductViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin,\
                      mixins.UpdateModelMixin, GenericViewSet):
@@ -15,15 +17,11 @@ class ProductViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Retr
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-
-    def get_permissions(self):
-        if self.request.method.upper() in ['HEAD', 'OPTIONS']:
-            return []
-        if self.request.method.upper() in ['GET']:
-            return [IsAuthenticated()]
-        if self.request.method.upper() in ['POST', 'PATCH', 'PUT', 'DELETE']:
-            return [AllowAdminOnly()]
-    
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ['price', 'name']
+    ordering_fields = ['price']
+    search_fields = ['name', 'display_name']
+ 
     def get_serializer_class(self):
         return super().get_serializer_class()
 
